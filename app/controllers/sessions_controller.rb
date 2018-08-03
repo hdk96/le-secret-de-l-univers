0c234c6059e393900626d9e14d40fe741ec651d2
hdk96 <amir.madi34@gmail.com>
2018-08-03 04:23:36 +0000

    Updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,6 @@ class SessionsController < ApplicationController
   def destroy
       session[:user_id] = nil
       flash[:success] = "Aurevoir !"
-      redirect_to root_path
+      redirect_to home_path
   end
 end
